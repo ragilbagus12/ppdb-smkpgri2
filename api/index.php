fbc877f6131bd3e9dfc330c2709b6c1ffd03c6e6
hanfik27 <hanfik27@gmail.com>
2026-09-22 19:13:15 +0700

    Update API Lagi

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Gunakan require (bukan require_once) supaya tidak return `true`
+// saat container di-reuse pada request berikutnya
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
